cc6ebc25d003eb5cd14d8855e965c521cfc996fb
omicronz <omcdash@gmail.com>
2016-06-28 14:37:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var config = {
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6Lc4vyMTAAAAALXkPreZL7kN5wPaCJ2be3Vxah86',  // <----- EDIT ME!
-  redirect_uri: 'https:/omicronz.github.io',
+  redirect_uri: 'https://omicronz.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: '//socket.moneypot.com',
